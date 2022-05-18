02b0c4e8af6482cfa66908a86ad133ca706ff335
jisokoo <abarbancon29@gmail.com>
2022-05-18 11:02:38 +0200

    add all the array

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
-const hello = 'Hello there';
-console.log(hello);
+const day = () => {return 1 + Math.floor(Math.random() * 30)};
+
+const month = () => {
+    const monthList = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+    const i = Math.floor(Math.random() * 11);
+    return monthList[i];
+};
+
+const year = () => {return 2022 + Math.floor(Math.random() * 50);};
+
+const date = () => {return day() + ' ' + month() + ' ' + year()};
+
+const whenArray = [`On the ${date()}`, `Around the ${day()} of ${month()}`, `In ${month()}`, `During year ${year()}`];
+
+const whoArray = ["you", "one of your family number", 'your best friend', 'that guy you hate', 'your colleague', 'your crush', 'your neighboor', 'the President of the United States'];
+
+const actionArray = ['probably die because of', 'discover', 'find', 'learn all about', 'come across', 'dream about', 'watch a strange movie about', 'have an insightful discussion about'];
+
+const objectArray = ['a yogurt', 'an apple', 'a weird bird', 'a WWII buried bomb', 'an old lady', 'a comet', 'a colorful mushroom', 'the truth', 'a heist taking place', 'your biggest fear', 'money', '... Hum, my vision stops here... I think it is related to something...'];
+
+const whereArray = ['in your house', 'in your garden', 'at Starbuck', 'at the grocery store', 'in a dark forest', '... not sure where, but I see the color blue'];
+
 
